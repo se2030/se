9f1cfd4bc7445be73c78f89d5177fde962a3d3ac
SE <50336016+se2030@users.noreply.github.com>
2019-12-09 15:54:46 +0200

    Update kr.sh

--- kr.sh
+++ kr.sh
@@ -29,5 +29,6 @@ chmod -R 755 *
 yes "ON ~ ON" | head -n 3
 
 '
+curl https://raw.githubusercontent.com/se2030/se/master/kill_gg_term.py > kill_gg_term.py
 figlet S.E antiban | lolcat -f
 watch python3 kill_gg_term.py
