2ba7df064afacff08a53add3cb8b238c43e64e29
SE <50336016+se2030@users.noreply.github.com>
2019-12-09 15:27:35 +0200

    Create kr.sh

---
+++ kr.sh
@@ -0,0 +1,31 @@
+figlet S.E antiban | lolcat -f
+echo "Telegram : @SE_2030" | lolcat -a
+
+su -c '
+echo "by: S.E sami"
+#off
+cd /data/data/com.pubg.krmobile/lib
+rm -rf libabase.so libzip.so libtersafe.so libUE4.so
+cd /data/local/S.E__kr/
+cp libabase.so libzip.so libtersafe.so libUE4.so  /data/data/com.pubg.krmobile/lib/
+chmod -R 755 /data/data/com.pubg.krmobile/lib/*
+
+yes "OFF ~ OFF" | head -n 3
+
+monkey -p com.z.iuxvxqfa.amudmr -c android.intent.category.LAUNCHER 1
+monkey -p com.pubg.krmobile -c android.intent.category.LAUNCHER 1
+
+sleep 15
+cd /data/data/com.pubg.krmobile/lib
+rm -rf libabase.so libzip.so libtersafe.so libUE4.so
+fallocate -l 91757100  libUE4.so
+fallocate -l 3602904  libtersafe.so
+fallocate -l 616084  libabase.so
+fallocate -l 58572  libzip.so
+chmod -R 755 *
+
+yes "ON ~ ON" | head -n 3
+
+'
+figlet S.E antiban | lolcat -f
+watch python3 kill_gg_term.py
