3cb4544371d8573613e2f3e8fdc87db6ce8ccf1a
SE <50336016+se2030@users.noreply.github.com>
2019-12-09 15:03:12 +0200

    Create kill_gg_term.py

---
+++ kill_gg_term.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import mmap,os
+os.system("su -c ps > ps_A.txt")
+with open('ps_A.txt', 'rb', 0) as file, \
+    mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
+    if s.find(b'com.pubg.krmobile') != -1:
+        print("true")
+    else:
+        os.system("pid=$(su -c pidof com.z.iuxvxqfa.amudmr com.termux ) ;su -c kill $pid ")
