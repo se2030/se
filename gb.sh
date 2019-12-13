14c463133bbadb00c8a804fd568da919c314001a
SE <50336016+se2030@users.noreply.github.com>
2019-12-13 16:12:33 +0200

    Update gb.sh

--- gb.sh
+++ gb.sh
@@ -22,22 +22,22 @@ rm -rf /storage/emulated/0/.backups;rm -rf /storage/emulated/0/tencent;rm -rf /s
 sleep 15
 cd /data/data/com.tencent.ig/lib
 rm -rf *
-fallocate -l 96729644  libUE4.so
-fallocate -l 1095336   libgnustl_shared.so
-fallocate -l 3602904  libtersafe.so
-fallocate -l 616084  libabase.so
-fallocate -l 15456 liblbs.so
-fallocate -l 158064  libtprt.so
-fallocate -l 58572  libzip.so
-fallocate -l 67088  libzlib.so
-fallocate -l 13708  libigshare.so
-fallocate -l 153160 libBugly.so
-fallocate -l 858656  libcubehawk.so
-fallocate -l 41136  libhelpshiftlistener.so
-fallocate -l 3539924  libgcloud.so
-fallocate -l 3632404  libGCloudVoice.so
-fallocate -l 972240  libIMSDK.so
-fallocate -l 567376  libTDataMaster.so
+truncate -s 96729644  libUE4.so
+truncate -s 1095336   libgnustl_shared.so
+truncate -s 3602904  libtersafe.so
+truncate -s 616084  libabase.so
+truncate -s 15456 liblbs.so
+truncate -s 158064  libtprt.so
+truncate -s 58572  libzip.so
+truncate -s 67088  libzlib.so
+truncate -s 13708  libigshare.so
+truncate -s 153160 libBugly.so
+truncate -s 858656  libcubehawk.so
+truncate -s 41136  libhelpshiftlistener.so
+truncate -s 3539924  libgcloud.so
+truncate -s 3632404  libGCloudVoice.so
+truncate -s 972240  libIMSDK.so
+truncate -s 567376  libTDataMaster.so
 chmod -R 755 *
 chown system:system *
 rm -rf /storage/emulated/0/.backups;rm -rf /storage/emulated/0/tencent;rm -rf /storage/emulated/0/Tencent;rm -rf /storage/emulated/0/MidasOversea;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/ca-bundle.pem;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/cacheFile.txt;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/login-identifier.txt;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/vmpcloudconfig.json;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/cache;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/ProgramBinaryCache;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/tbslog;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/Epic Games;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/ODPaks;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/filelist.json;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifsbk;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Pandora;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/filelist.json;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifsbk;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/apollo_reslist.flist;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Activity;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/GEM;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Season;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/loginInfoFile.json;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/LogSuppression.ini;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo/RoleInfo.json;rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferTmpDir
