5e6edf869e26a90d0d5f9afbe8ad53deafd2230a
SE <50336016+se2030@users.noreply.github.com>
2019-12-17 00:28:28 +0200

    Update kr_install.sh

--- kr_install.sh
+++ kr_install.sh
@@ -4,9 +4,10 @@ termux-setup-storage
 apt list > ~/apt_list.txt
 pip list > ~/pip_list.txt
 su -c ls /data/local/ > ~/ls_local.txt
-rm -rf ~/.shortcuts/* ;mkdir -p ~/.shortcuts/ cd ~/.shortcuts/
+rm -rf ~/.shortcuts
+mkdir ~/.shortcuts/
 printf "curl -s https://raw.githubusercontent.com/se2030/se/master/kr_v2.sh | bash -s" > kr_v2.sh
-
+mv ~/kr_v2.sh ~/.shortcuts/
 if grep 'python' ~/apt_list.txt
 then
 	echo "python ok "
