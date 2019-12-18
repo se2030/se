1d8ebbe5c85af292b5cd2d9f5d21ee373ca991c4
SE <50336016+se2030@users.noreply.github.com>
2019-12-18 18:55:05 +0200

    Update vn_v2.sh

--- vn_v2.sh
+++ vn_v2.sh
@@ -7,7 +7,7 @@ echo "by: S.E sami"
 #off
 cd /data/data/com.vng.pubgmobile/lib
 rm -rf libabase.so libzip.so libtersafe.so libUE4.so
-cd /data/local/S.E_kr_v2/
+cd /data/local/S.E_vn_v2/
 cp libabase.so libzip.so libtersafe.so libUE4.so  /data/data/com.vng.pubgmobile/lib/
 chmod -R 755 /data/data/com.vng.pubgmobile/lib/*
 
