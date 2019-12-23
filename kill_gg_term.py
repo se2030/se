abd4ff261628a37558b3d2d54cd90e4f77b0bfeb
SE <50336016+se2030@users.noreply.github.com>
2019-12-23 19:46:42 +0200

    Update kill_gg_term.py

--- kill_gg_term.py
+++ kill_gg_term.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import mmap,os
 os.system("su -c ps > ps_A.txt")
+os.system("printf "StatEventType::APP_LAUNCH=false\nStatEventType::LOADING_COMPLETED=false\nStatEventType::TUTORIAL_START=false " > /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag")
 #os.system ("rm -rf /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag")
 with open('ps_A.txt', 'rb', 0) as file, \
     mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
