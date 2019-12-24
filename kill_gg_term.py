f38daa0a9a0b1cc1772505acb76b92b2e7535b49
SE <50336016+se2030@users.noreply.github.com>
2019-12-24 15:39:28 +0200

    Update kill_gg_term.py

--- kill_gg_term.py
+++ kill_gg_term.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 import mmap,os
 os.system("su -c ps > ps_A.txt")
-#os.system("printf "StatEventType::APP_LAUNCH=false\nStatEventType::LOADING_COMPLETED=false\nStatEventType::TUTORIAL_START=false " > /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag")
-#os.system ("rm -rf /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag")
+os.system("if grep "true" /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag ;then       printf "StatEventType::APP_LAUNCH=false\nStatEventType::LOADING_COMPLETED=false\nStatEventType::TUTORIAL_START=false " > /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag ;fi")
 with open('ps_A.txt', 'rb', 0) as file, \
     mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
     if s.find(b'com.pubg.krmobile') != -1:
