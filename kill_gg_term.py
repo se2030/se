af0c206176f2893531eb19abe78f98e75ab3c1e2
SE <50336016+se2030@users.noreply.github.com>
2019-12-25 23:29:55 +0200

    Update kill_gg_term.py

--- kill_gg_term.py
+++ kill_gg_term.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import mmap,os
 
-os.system("su -c ps > ps_A.txt ;su -c ps -A > ps_B.txt ;su -c cat ps* > ps.txt")
+os.system("su -c ps > ps_A.txt ;su -c ps -A > ps_B.txt ;su -c cat ps* > ps.txt;su -c rm -rf ps_*")
 #os.system("if grep "true" /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag ;then       printf "StatEventType::APP_LAUNCH=false\nStatEventType::LOADING_COMPLETED=false\nStatEventType::TUTORIAL_START=false " > /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag ;fi")
 with open('ps.txt', 'rb', 0) as file, \
     mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
