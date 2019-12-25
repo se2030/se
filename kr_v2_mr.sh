6f93a9f6acc5978aebede6afa55b7a957fabb4c8
SE <50336016+se2030@users.noreply.github.com>
2019-12-25 21:39:25 +0200

    Update kr_v2_mr.sh

--- kr_v2_mr.sh
+++ kr_v2_mr.sh
@@ -2,33 +2,27 @@ rm -rf ~/kill_gg_term.py
 figlet S.E antiban | lolcat -f
 echo "Telegram : @SE_2030" | lolcat -a
 
+
 su -c '
-echo "by: S.E sami"
-#off
 cd /data/data/com.pubg.krmobile/lib
-rm -rf libabase.so libzip.so libtersafe.so libUE4.so
+rm -rf *
 cd /data/local/S.E_kr_v2/
-cp libabase.so libzip.so libtersafe.so libUE4.so  /data/data/com.pubg.krmobile/lib/
+cp *  /data/data/com.pubg.krmobile/lib/
 chmod -R 755 /data/data/com.pubg.krmobile/lib/*
-
-yes "OFF ~ OFF" | head -n 3
-
-monkey -p com.z.iuxvxqfa.amudmr -c android.intent.category.LAUNCHER 1
-monkey -p com.pubg.krmobile -c android.intent.category.LAUNCHER 1
-
-sleep 15
-cd /data/data/com.pubg.krmobile/lib
-rm -rf libabase.so libzip.so libtersafe.so libUE4.so
-truncate -s 96729644  libUE4.so
-truncate -s 3602904  libtersafe.so
-truncate -s 616084  libabase.so
-truncate -s 67088  libzip.so
-chmod -R 755 *
-
-yes "ON ~ ON" | head -n 3
+chown system:system /data/data/com.pubg.krmobile/lib/*
 
 '
-while true;do printf "StatEventType::APP_LAUNCH=false\nStatEventType::LOADING_COMPLETED=false\nStatEventType::TUTORIAL_START=false " > /sdcard/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag ;done &
+
+echo "by: S.E sami"
+if ls -s libtersafe.so | grep "3448 libtersafe.so"
+then
+     printf "\n ok \n"
+else
+     wget https://github.com/se2030/se/raw/master/libtersafe.so
+     su -c ' rm -rf /data/data/com.pubg.krmobile/lib/libtersafe.so
+     cp libtersafe.so /data/data/com.pubg.krmobile/lib/
+     chmod -R 755 /data/data/com.pubg.krmobile/lib/libtersafe.so'
+
 curl https://raw.githubusercontent.com/se2030/se/master/kill_gg_term.py > kill_gg_term.py
 figlet S.E antiban | lolcat -f
-watch python3 ~/kill_gg_term.py
+watch -n 0 python3 ~/kill_gg_term.py
