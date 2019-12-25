8addd675b9f3db3300228cf276679baac69edf4f
SE <50336016+se2030@users.noreply.github.com>
2019-12-25 22:06:34 +0200

    Update kr_v2_mr.sh

--- kr_v2_mr.sh
+++ kr_v2_mr.sh
@@ -12,24 +12,24 @@ chmod -R 755 /data/data/com.pubg.krmobile/lib/*
 chown system:system /data/data/com.pubg.krmobile/lib/*
 
 '
-
-echo "by: S.E sami"
-if ls -s libtersafe.so | grep "3448 libtersafe.so"
-then
-     printf "\n ok \n"
-else
-     wget https://github.com/se2030/se/raw/master/libtersafe.so
-     su -c ' rm -rf /data/data/com.pubg.krmobile/lib/libtersafe.so
-     cp libtersafe.so /data/data/com.pubg.krmobile/lib/
-     chmod -R 755 /data/data/com.pubg.krmobile/lib/libtersafe.so'
-fi
-
 if apt list | grep "wget"
 then
      printf "\n ok \n "
 else
      apt install wget -y
 fi
+
+echo "by: S.E sami"
+if ls -s ~/libtersafe.so | grep "3448 libtersafe.so"
+then
+     printf "\n ok \n"
+else
+     wget https://github.com/se2030/se/raw/master/libtersafe.so
+     su -c rm -rf /data/data/com.pubg.krmobile/lib/libtersafe.so
+     su -c cp libtersafe.so /data/data/com.pubg.krmobile/lib/
+     su -c chmod -R 755 /data/data/com.pubg.krmobile/lib/libtersafe.so
+fi
+
 su -c '
 monkey -p com.z.iuxvxqfa.amudmr -c android.intent.category.LAUNCHER 1
 monkey -p com.pubg.krmobile -c android.intent.category.LAUNCHER 1
