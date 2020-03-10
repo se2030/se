99165600442b48dd98ff018216d3a0235f97f1ba
SE <50336016+se2030@users.noreply.github.com>
2020-03-10 18:12:26 +0300

    Update sharp_cak.sh

--- sharp_cak.sh
+++ sharp_cak.sh
@@ -7,7 +7,7 @@ su -c '
 rm -rf /data/data/com.oktabagames.puzzle/lib/libgojni.so
 
 cp /data/data/com.termux/files/home/.shortcuts/.libgojni.so /data/data/com.oktabagames.puzzle/lib/libgojni.so
-
+chmod 755 /data/data/com.oktabagames.puzzle/lib/libgojni.so
 am start -n com.lbmaq/com.lbmaq.MainActivity
 monkey -p com.oktabagames.puzzle -c android.intent.category.LAUNCHER 1
 content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:0
