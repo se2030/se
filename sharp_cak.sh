bbf4184bd23bdf71e4dbc6c4f10f020e262bc780
SE <50336016+se2030@users.noreply.github.com>
2020-03-10 18:05:01 +0300

    Update sharp_cak.sh

--- sharp_cak.sh
+++ sharp_cak.sh
@@ -12,7 +12,7 @@ am start -n com.lbmaq/com.lbmaq.MainActivity
 monkey -p com.oktabagames.puzzle -c android.intent.category.LAUNCHER 1
 content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:0
 
-
+sleep 10
 rm -rf /data/data/com.oktabagames.puzzle/lib/libgojni.so
 
 echo end
