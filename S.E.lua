34d6f8f02a3068db9bc75e7bc56217fe2c7224ae
SE <50336016+se2030@users.noreply.github.com>
2020-06-23 14:44:32 +0300

    Update S.E.lua

--- S.E.lua
+++ S.E.lua
@@ -63,6 +63,7 @@ function lobby()
     "ايملوك 150m",
     "  ازالة حشيش خريطة ايرنجل ",
     "منع انتشار الرصاص",
+    "منظور واسع",
     "رجوع",
   }, nil, "sami S.E \n telegram: @SE_2030")
   if MN0 == nil then
@@ -87,6 +88,9 @@ function lobby()
     end
     if MN0[7] == true then
       smailcrosshair()
+    end
+    if MN0[8] == true then
+      ipad150()
     end
     if MN0[8] == true then
       HOME()
@@ -253,6 +257,114 @@ gg.setRanges(gg.REGION_C_DATA)
     gg.editAll("90", gg.TYPE_FLOAT)
 gg.toast("No Recoil 100% ᴀcтιvᴀтᴇᴅ! 0.17.0")
 --50% + 100% = 100% ++
+end
+
+function ipad150()
+GCR = gg.clearResults
+function split(szFullString, szSeparator)
+	local nFindStartIndex = 1
+	local nSplitIndex = 1
+	local nSplitArray = {}
+	while true do
+		local nFindLastIndex = string.find(
+			szFullString,
+			szSeparator,
+			nFindStartIndex
+		)
+		if not nFindLastIndex then
+			nSplitArray[nSplitIndex] = string.sub(
+				szFullString,
+				nFindStartIndex,
+				string.len(szFullString)
+			)
+			break
+		end
+		nSplitArray[nSplitIndex] = string.sub(
+			szFullString,
+			nFindStartIndex,
+			nFindLastIndex - 1
+		)
+		nFindStartIndex = nFindLastIndex + string.len(szSeparator)
+		nSplitIndex = nSplitIndex + 1
+	end
+	return nSplitArray
+end
+function xgxc(szpy, qmxg)
+	for x = 1, #(qmxg) do
+		xgpy = szpy + qmxg[x]["offset"]
+		xglx = qmxg[x]["type"]
+		xgsz = qmxg[x]["value"]
+		gg.setValues({[1] = {address = xgpy, flags = xglx, value = xgsz}})
+		xgsl = xgsl + 1
+	end
+end
+function xqmnb(qmnb)
+	gg.clearResults()
+	gg.setRanges(qmnb[1]["memory"])
+	gg.searchNumber(qmnb[3]["value"], qmnb[3]["type"])
+	if gg.getResultCount() == 0 then
+		gg.toast(qmnb[2]["name"] .. "Open failed")
+	else
+		gg.refineNumber(qmnb[3]["value"], qmnb[3]["type"])
+		gg.refineNumber(qmnb[3]["value"], qmnb[3]["type"])
+		gg.refineNumber(qmnb[3]["value"], qmnb[3]["type"])
+		if gg.getResultCount() == 0 then
+			gg.toast(qmnb[2]["name"] .. "Open failed")
+		else
+			sl = gg.getResults(999999)
+			sz = gg.getResultCount()
+			xgsl = 0
+			if sz > 999999 then
+				sz = 999999
+			end
+			for i = 1, sz do
+				pdsz = true
+				for v = 4, #(qmnb) do
+					if pdsz == true then
+						pysz = {}
+						pysz[1] = {}
+						pysz[1].address = sl[i].address + qmnb[v]["offset"]
+						pysz[1].flags = qmnb[v]["type"]
+						szpy = gg.getValues(pysz)
+						pdpd = qmnb[v]["lv"] .. ";" .. szpy[1].value
+						szpd = split(pdpd, ";")
+						tzszpd = szpd[1]
+						pyszpd = szpd[2]
+						if tzszpd == pyszpd then
+							pdjg = true
+							pdsz = true
+						else
+							pdjg = false
+							pdsz = false
+						end
+					end
+				end
+				if pdjg == true then
+					szpy = sl[i].address
+					xgxc(szpy, qmxg)
+					xgjg = true
+				end
+			end
+			if xgjg == true then
+				gg.toast(qmnb[2]["name"] .. "Modified " .. xgsl .. " Values")
+			else
+				gg.toast(qmnb[2]["name"] .. "Open failed")
+			end
+		end
+	end
+end
+
+qmnb = {
+	{["memory"] = 8},
+	{["name"] = "Wide View CD xClusive "},
+	{["value"] = 3.612870300195559E-37, ["type"] = 16},
+	{["lv"] = 360.0, ["offset"] = -8, ["type"] = 16},
+}
+qmxg = {
+	{["value"] = 254.70928955078125, ["offset"] = -8, ["type"] = 16},
+}
+xqmnb(qmnb)
+
 end
 
 --game function
