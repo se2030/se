f95deb251499d0070e68a0d085d2f6f8e1366397
SE <50336016+se2030@users.noreply.github.com>
2020-08-19 20:53:54 +0300

    Update project_base_pak.sh

--- project_base_pak.sh
+++ project_base_pak.sh
@@ -1,9 +1,11 @@
+echo 'Telegram: @SE_2030'
 su -c '
 rm -rf /data/media/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_*
 rm -rf /data/media/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SrcVersion.ini
 rm -rf /data/media/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
 
+iptables -D OUTPUT -s file.igamecj.com -j DROP
+iptables -D INPUT -s file.igamecj.com -j DROP
 iptables -A OUTPUT -s file.igamecj.com -j DROP
 iptables -A INPUT -s file.igamecj.com -j DROP
-
 '
