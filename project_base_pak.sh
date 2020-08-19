c92e79a11ea343c8104a363882c7e96cb1ee3b6d
SE <50336016+se2030@users.noreply.github.com>
2020-08-19 21:32:00 +0300

    Update project_base_pak.sh

--- project_base_pak.sh
+++ project_base_pak.sh
@@ -4,9 +4,7 @@ rm -rf /data/media/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtr
 rm -rf /data/media/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SrcVersion.ini
 rm -rf /data/media/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
 
-iptables -D OUTPUT -s file.igamecj.com -j DROP
-iptables -D INPUT -s file.igamecj.com -j DROP
-iptables -A OUTPUT -s file.igamecj.com -j DROP
-iptables -A INPUT -s file.igamecj.com -j DROP
+iptables -t nat -D OUTPUT -p all -d file.igamecj.com -j DNAT --to-destination 0.0.0.1
+iptables -t nat -A OUTPUT -p all -d file.igamecj.com -j DNAT --to-destination 0.0.0.1
 am start -n com.tencent.ig/com.epicgames.ue4.SplashActivity
 '
