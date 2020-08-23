8e4f39c9c28c65e7426468dd7bf98276d6d1b574
SE <50336016+se2030@users.noreply.github.com>
2020-08-23 22:55:37 +0300

    Update project_base_pak.sh

--- project_base_pak.sh
+++ project_base_pak.sh
@@ -1,8 +1,8 @@
 echo 'Telegram: @SE_2030'
 su -c '
-rm -rf /data/media/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_*
-rm -rf /data/media/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SrcVersion.ini
-rm -rf /data/media/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
+rm -rf /data/media/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_*
+rm -rf /data/media/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SrcVersion.ini
+rm -rf /data/media/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
 
 iptables -D OUTPUT -p tcp --destination-port 8086 -j DROP
 iptables -D INPUT -p tcp --destination-port 8086 -j DROP
@@ -18,5 +18,5 @@ iptables -A INPUT -p tcp --destination-port 8085 -j DROP
 iptables -A OUTPUT -s gcloud-versvr.igamecj.com -j DROP
 iptables -A INPUT -s gcloud-versvr.igamecj.com -j DROP
 
-am start -n com.tencent.ig/com.epicgames.ue4.SplashActivity
+am start -n com.pubg.krmobile/com.epicgames.ue4.SplashActivity
 '
